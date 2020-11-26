bf47ed3c749d7c88b4c115671ed50151ed720305
Laura <almasilaura@outlook.com>
2020-11-26 09:01:29 +0000

    that's the homework done

---
+++ start_code/src/compare.py
@@ -0,0 +1,10 @@
+def compare(num1, num2):
+    if num1 < num2:
+        return f"{num1} is greater than {num2}"
+
+def compare(num1, num2):
+
+    if num1 == num2:
+        return "numbers are the same"
+    if num1 < num2:
+        return f"{num1} is less than {num2}"
